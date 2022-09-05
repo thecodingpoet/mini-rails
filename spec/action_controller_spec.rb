29dd211b63652568e3e1f9264ffe117f27ae6909
Tomi Duyile <thecodingpoet@gmail.com>
2022-09-05 18:43:15 +0100

    Implement `redirect_to`

--- spec/action_controller_spec.rb
+++ spec/action_controller_spec.rb
@@ -1,28 +1,32 @@
 RSpec.describe ActionController do
-  class TestController < ActionController::Base
-    before_action :callback, only: [:show]
-    before_action :second_callback, except: [:show]
-
-    def index
-      response << "index"
+  describe "Test Controller" do 
+    class TestController < ActionController::Base
+      before_action :callback, only: [:show]
+      before_action :second_callback, except: [:show, :redirect]
+  
+      def index
+        response << "index"
+      end
+  
+      def show
+        response << "show"
+      end
+  
+      def redirect 
+        redirect_to "/"
+      end
+  
+      private 
+  
+      def callback
+        response << "callback"
+      end
+  
+      def second_callback
+        response << "second_callback"
+      end
     end
-
-    def show
-      response << "show"
-    end
-
-    private 
-
-    def callback
-      response << "callback"
-    end
-
-    def second_callback
-      response << "second_callback"
-    end
-  end
-
-  describe TestController do 
+  
     describe "#index" do
       it "works" do
         controller = TestController.new
@@ -45,9 +49,26 @@ RSpec.describe ActionController do
         expect(controller.response).to eq(["callback", "show", "second_callback", "index"])
       end
     end
+
+    
+    describe "#redirect" do
+      class Response
+        attr_accessor :status, :location, :body
+      end
+    
+      it "works" do 
+        controller = TestController.new
+        controller.response = Response.new
+        controller.process(:redirect)
+
+        expect(controller.response.status).to eq(302)
+        expect(controller.response.location).to eq("/")
+        expect(controller.response.body).to eq(["You are being redirected"])
+      end
+    end
   end
 
-  describe PostsController do
+  describe "Real Controller" do
     class Request
       def params
         { 'id': 1 }
@@ -58,6 +79,8 @@ RSpec.describe ActionController do
       controller = PostsController.new 
       controller.request = Request.new
       controller.process(:show)
+
+      expect(controller.instance_variable_get(:@post)).to be_instance_of(Post)
     end
   end
 end
